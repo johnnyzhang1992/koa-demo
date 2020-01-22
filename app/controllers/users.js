b8aa9d3d90ebfd5ee713f51164e1beef8689b64e
zq19920306 <1113638674@qq.com>
2020-01-22 10:48:13 +0800

    update

--- app/controllers/users.js
+++ app/controllers/users.js
@@ -8,6 +8,36 @@ class UserController {
 	// 		.skip(page * perPage);
 	// }
 
+	async findOne(ctx) {
+		//从ctx中读取get传值
+		console.log(ctx.query); //{ aid: '123' } 获取的是对象, 用的最多的方式  **推荐
+		console.log(ctx.querystring); //aid=123&name=zhangsan, 获取的是一个字符串
+		console.log(ctx.url); //获取url地址
+		//ctx里面的request里面获取get传值
+		console.log(ctx.request.url);
+		console.log(ctx.request.query); //{ aid: '123', name: 'zhangsan' }  对象
+		console.log(ctx.request.querystring); //aid=123&name=zhangsan
+		// params
+		console.log(ctx.params);
+		const { id } = ctx.params;
+		// 参数验证
+		// ctx.verifyParams({
+		// 	name: { type: "string", required: true }
+		// 	// age: {type: 'number', required: false}
+		// });
+		ctx.body = id === 23 ? {
+			user: {
+				name: "jake",
+				age: 23,
+				id
+			},
+			status_code: 200
+		} : {
+				message: 'user not find',
+				status_code: 200
+		};
+	}
+
 	async create(ctx) {
 		ctx.verifyParams({
 			name: { type: "string", required: true },
