78461ff5143ed435ab2b00b62f1f00cf1b70695d
johnnyzhang1992 <zq19920306@gmail.com>
2020-03-11 00:09:36 +0800

    update

--- app/controllers/users.js
+++ app/controllers/users.js
@@ -18,47 +18,26 @@ mongoose
 	});
 
 class UserController {
-	// async find(ctx) {
-	// 	const { per_page = 10 } = ctx.query;
-	// 	const page = Math.max(ctx.query.page * 1, 1) - 1;
-	// 	const perPage = Math.max(per_page * 1, 1);
-	// 	ctx.body = await User.find({ name: new RegExp(ctx.query.q) })
-	// 		.limit(perPage)
-	// 		.skip(page * perPage);
-	// }
-
+	/**
+	 * @description根据条件查询用户
+	 * @param {*} ctx
+	 */
 	async findOne(ctx) {
-		//从ctx中读取get传值
-		console.log(ctx.query); //{ aid: '123' } 获取的是对象, 用的最多的方式  **推荐
-		console.log(ctx.querystring); //aid=123&name=zhangsan, 获取的是一个字符串
-		console.log(ctx.url); //获取url地址
-		//ctx里面的request里面获取get传值
-		console.log(ctx.request.url);
-		console.log(ctx.request.query); //{ aid: '123', name: 'zhangsan' }  对象
-		console.log(ctx.request.querystring); //aid=123&name=zhangsan
-		// params
-		console.log(ctx.params);
+		// 路由匹配
 		const { id } = ctx.params;
-		// 参数验证
-		// ctx.verifyParams({
-		// 	name: { type: "string", required: true }
-		// 	// age: {type: 'number', required: false}
-		// });
-		ctx.body =
-			id === 23
-				? {
-						user: {
-							name: "jake",
-							age: 23,
-							id
-						},
-						status_code: 200
-				  }
-				: {
-						message: "user not find",
-						status_code: 200
-				  };
+		// URL 参数
+		const query = ctx.request.query;
+		ctx.body = {
+			status_code: 200,
+			query,
+			id
+		};
 	}
+
+	/**
+	 * @description 创建用户
+	 * @param {*} ctx
+	 */
 
 	async create(ctx) {
 		ctx.verifyParams({
@@ -85,12 +64,14 @@ class UserController {
 			await newUser
 				.save()
 				.then(user => {
-					ctx.body = user;
+					ctx.body = user || {
+						message: "用户创建失败"
+					};
 				})
 				.catch(err => {
 					console.log(err);
 					ctx.body = {
-						message: "create fail"
+						message: "用户创建失败"
 					};
 				});
 		}
